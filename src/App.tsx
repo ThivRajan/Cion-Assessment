c26328f38069a436527174ce54cc3d9818c4148e
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-06-02 00:44:55 -0400

    Add app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import './App.css';
+import { Icon } from '@iconify/react';
+import arrowDownAlt2 from '@iconify-icons/dashicons/arrow-down-alt2';
 
 import ChatList from './components/ChatList/ChatList';
 import ChatView from './components/ChatView/ChatView';
 
+import samplePic from './assets/sample-dp.png';
+
 const App = (): JSX.Element => {
 	return (
 		<div className="App">
-			<ChatList />
-			<ChatView />
+			<div className="app-header">
+				<h2>Chat App</h2>
+				<div className="user-details">
+					<img src={samplePic} alt="Profile Picture" className="user-profile-pic" />
+					Test User
+					<Icon icon={arrowDownAlt2} className="profile-arrow-icon" />
+				</div>
+			</div>
+			<div className="app-contents">
+				<ChatList />
+				<ChatView />
+			</div>
 		</div>
 	);
 };
